983ef55de8083ff65dfc8d9a714e6ab162d8feeb
kaitt-cwf <katia.reyes.inzunza@gmail.com>
2026-04-15 16:22:39 -0400

    edicion a Promedio

--- Promedio.py
+++ Promedio.py
@@ -1,10 +1,20 @@
-
+# Se define donde se suman las notas 
 suma_notas = 0
 
+# Se le pide al usuario la cantidad de notas que quiere ingresar
 cant = int(input("¿Cuantas notas quiere ingresar? \n"))
+
 
 for a in range(cant):
     nota = float(input("Ingrese su nota: "))
     suma_notas = suma_notas + nota
 
-print("Su promedio es", suma_notas/cant)
+prom = suma_notas/cant
+
+print("Su promedio es", prom)
+
+if prom < 4:
+    print("Usted ha reprobado.")
+else:
+    print("Usted ha aprobado.")
+
